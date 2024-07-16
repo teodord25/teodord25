9c3f4b9b40bf75cc1dc019771eb67d6fef99c7ad
tedord25 <djuric.teodor25@gmail.com>
2024-07-16 21:01:34 +0200

    change entire script to just distribution of commits per lang piechart

--- analyze_contributions.py
+++ analyze_contributions.py
@@ -1,14 +1,9 @@
 # NOTE: this is pretty scuffed so don't expect it to be 100% accurate
 
 from datetime import datetime, timedelta
-from typing import List
 import requests
 import os
-import json
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator  # Added to handle y-axis ticks
-import numpy as np
-import pprint
 
 HEADERS = {
     'Authorization': f'token {os.getenv("GITHUB_TOKEN")}',
@@ -44,35 +39,6 @@ USER = "teodord25"
 EMAIL = "djuric.teodor25@gmail.com"
 
 
-def compute_lines(lines):
-    added = sum(
-        1 for line in lines
-        if line.startswith('+') and not line.startswith('+++')
-    )
-    removed = sum(
-        1 for line in lines
-        if line.startswith('-') and not line.startswith('---')
-    )
-
-    lines_changed = added + removed
-
-    return lines_changed
-
-
-def parse_file(file) -> List[str]:
-    filename = file["filename"]
-
-    file_ext = os.path.splitext(filename)[1]
-
-    if file_ext not in LANGUAGES or file_ext == ".md":  # ignore markdown files
-        return []
-
-    if 'patch' not in file:
-        return []
-
-    return file["patch"].split('\n')
-
-
 def get_repos():
     repos = []
     page = 1
@@ -89,17 +55,35 @@ def get_repos():
     return repos
 
 
-def compute_summary_for_week(repos, start_date):
+def plot_pie_chart(data):
+    labels = data.keys()
+    sizes = data.values()
+    colors = plt.get_cmap('tab20')(range(len(data)))
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, colors=colors)
+    ax.axis('equal')
+
+    plt.title('Commit Distribution by Programming Language')
+    plt.show()
+    plt.savefig('commit_distribution.png')
+
+
+def compute_summary(repos):
     summary = {}
-    formatted_date = start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+    seven_days_ago = (datetime.today() - timedelta(days=7)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
     for owner, name in repos:
         if name == ".github-private" or name == ".github":
             continue
 
-        url = f"{URL_BASE}/repos/{owner}/{name}/commits?since={formatted_date}&until={start_date + timedelta(days=7)}"
+        url = f"{URL_BASE}/repos/{owner}/{name}/commits?since={seven_days_ago}"
         response = requests.get(url, headers=HEADERS).json()
 
-        if isinstance(response, dict) and response.get("message") == "Not Found":
+        if response == []:
+            continue
+
+        if isinstance(response, dict):
             continue
 
         for commit in response:
@@ -112,147 +96,32 @@ def compute_summary_for_week(repos, start_date):
 
             sha = commit["sha"]
             url = f"{URL_BASE}/repos/{owner}/{name}/commits/{sha}"
-            response = requests.get(url, headers=HEADERS,
-                                    allow_redirects=True).json()
-            files = response["files"]
+            response = requests.get(
+                url, headers=HEADERS, allow_redirects=True
+            ).json()
+            files = response.get("files", [])
+
+            languages_in_commit = set()
+
             for file in files:
-                lines = parse_file(file)
-                if not lines:
-                    continue
+                extension = os.path.splitext(file["filename"])[1]
+                language = LANGUAGES.get(extension)
 
-                language = LANGUAGES.get(os.path.splitext(file["filename"])[1])
+                if language:
+                    languages_in_commit.add(language)
 
-                lines_changed = compute_lines(lines)
-
-                week = start_date.strftime('%Y-%m-%d')
-
-                if week not in summary:
-                    summary[week] = [
-                        {
-                            "language": language,
-                            "lines_changed": lines_changed
-                        }
-                    ]
-
-                elif len(summary[week]) == 0:
-                    summary[week].append(
-                        {
-                            "language": language,
-                            "lines_changed": lines_changed
-                        }
-                    )
-                elif language not in [commit["language"] for commit in summary[week]]:
-                    summary[week].append(
-                        {
-                            "language": language,
-                            "lines_changed": lines_changed
-                        }
-                    )
+            for language in languages_in_commit:
+                if language in summary:
+                    summary[language] += 1
                 else:
-                    for commit in summary[week]:
-                        if commit["language"] == language:
-                            commit["lines_changed"] += lines_changed
+                    summary[language] = 1
 
     return summary
 
 
-def load_data():
-    try:
-        with open('weekly_data.json', 'r') as file:
-            data = json.load(file)
-            if len(data) != 52:
-                raise Exception("Invalid data")
-            return data
-    except FileNotFoundError:
-        return []
-
-
-def save_data(data):
-    # keep the latest 52 weeks
-    if len(data) > 52:
-        data = data[-52:]
-    with open('weekly_data.json', 'w') as file:
-        json.dump(data, file, indent=4)
-
-
-def plot_stacked_bar_chart(data, y_limit=4000):
-    all_languages = set()
-    for week in data:
-        for date, entries in week.items():
-            for entry in entries:
-                all_languages.add(entry['language'])
-
-    dates = []
-    curr_date = ""
-    for week in data:
-        if week == {}:
-            dates.append((datetime.strptime(curr_date, '%Y-%m-%d') + timedelta(days=7)).strftime('%Y-%m-%d'))
-        for date, entries in week.items():
-            dates.append(date)
-            curr_date = date
-
-    weekly_summaries = {lang: [0] * len(dates) for lang in all_languages}
-
-    for i, week in enumerate(data):
-        for date, entries in week.items():
-            if date not in dates:
-                continue
-            index = dates.index(date)
-            for entry in entries:
-                weekly_summaries[entry['language']][index] += entry['lines_changed']
-
-    # plotting
-    fig, ax = plt.subplots(figsize=(10, 6))
-    bottom = np.zeros(len(dates))
-
-    total_lines_per_week = [sum(weekly_summaries[lang][i] for lang in all_languages) for i in range(len(dates))]
-
-    # color map
-    cmap = plt.get_cmap('tab20', len(all_languages))
-
-    colors = cmap(range(len(all_languages)))
-
-    language_colors = {lang: color for lang, color in zip(sorted(all_languages), colors)}
-
-    for lang in all_languages:
-        values = weekly_summaries[lang]
-        bars = ax.bar(dates, values, bottom=bottom, label=lang, color=language_colors[lang])
-        bottom += np.array(values)
-        # annotate bars that go off the chart
-        for i, (bar, total) in enumerate(zip(bars, total_lines_per_week)):
-            if bar.get_height() + bar.get_y() > y_limit:
-                ax.text(bar.get_x() + bar.get_width() / 2, y_limit - 50, f'{int(total)}', ha='center', va='top', color='white', fontsize=8, rotation=90)
-
-    ax.set_ylim([0, y_limit])
-    ax.set_xlabel('Weeks')
-    ax.set_ylabel('Lines Changed')
-
-    fig.suptitle('Weekly Lines Changed by Language', fontsize=16)
-    plt.title('Overflowing bars represent integrating and updating legacy code', fontsize=10, pad=10)
-
-    ax.legend(title='Languages', bbox_to_anchor=(1.05, 1), loc='upper left')
-
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-    plt.show()
-    plt.savefig('weekly_lines_changed.png')
-
-
-def update_data(data):
-    summary = compute_summary_for_week(get_repos(), datetime.today() - timedelta(days=7))
-
-    data.append(summary)  # add new week
-    data.pop(0)  # remove oldest week
-
-    save_data(data)
-
-
 def main():
-    data = load_data()
-
-    update_data(data)
-
-    plot_stacked_bar_chart(data)
+    summary = compute_summary(get_repos())
+    plot_pie_chart(summary)
 
 
 if __name__ == "__main__":
